5bfb776cc254850fa111c6b0450ea7f1827fa2fe
Blakely Cowan <154741753+JoCoRebel@users.noreply.github.com>
2025-05-02 19:11:44 -0400

    Update COWAN-gradebook.js

--- COWAN-gradebook.js
+++ COWAN-gradebook.js
@@ -13,8 +13,8 @@ function fetchGradeData() {
         if(xhr.readyState === XPathEvaluator.DONE){
             // Check if we're successful
             if(xhr.status !== 200){
-                console.error('Could not get grades.
-                    Status: ${xhr.status}');
+                console.error(`Could not get grades.
+                    Status: ${xhr.status}`);
             }
             // And then call the function to update the HTML with our data
             populateGradebook(JSON.parse(xhr.responseText));
@@ -29,7 +29,7 @@ function populateGradebook(data) {
     // This function will take the fetched grade data and populate the table
     console.log("Populating gradebook with data:", data);
     let tableElm = document.getElementById("gradebook"); // Get the gradebook table element
-        data.forEach(function(assignment){ //For each row of data we're passes in
+        data.forEach(function(assignment)){ //For each row of data we're passes in
             let row = document.createElement("tr"); //create a table row element
             let columns = []; // Handy place to stick the columns of information
             columns.name = document.createElement('td'); // The first columns table data will be the name
@@ -41,14 +41,13 @@ function populateGradebook(data) {
             columns.grade.appendChild(
                 // Just put the name in text, you could be fancy and figure out the letter grade here
                 // with either a bunch of conditions, or a Javascript "switch" statement
-                document.createTextNode(assignment.total_grade)
-            );
+                document.createTextNode(assignment.total_grade);            
             //  Add the table data columns to the table row
             row.appendChild(columns.name);
             row.appendChild(columns.grade);
             // Add the row to the table itself to make the data visible
             tableElm.appendChild(row);
-        });
+        };
 }
 
 // TODO REMOVE THIS
